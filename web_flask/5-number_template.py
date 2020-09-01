65b094c706f0babb7a6332c5352e60ab819f5aca
andreshugueth <andreshugueth@gmail.com>
2020-09-01 14:22:22 -0500

    web flask fixing task5 template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,8 +42,8 @@ def is_numeber(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """ display a HTML page only if n is an integer"""
+def numbersandtemplates(n):
+    """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
