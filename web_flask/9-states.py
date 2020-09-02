eedea34776f74dc9e3952fe79e74f68ae936bc79
andreshugueth <andreshugueth@gmail.com>
2020-09-02 14:10:39 -0500

    web flask task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import *
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states', defaults={'id': None}, strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def index(id):
+    """renders an HTML with a list of states"""
+    states = storage.all("State").values()
+    if id is not None:
+        state_id = 'State.{}'.format(id)
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
