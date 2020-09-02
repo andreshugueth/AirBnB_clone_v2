0c06152e2750108312d12b5589be57e3fa42d64b
andreshugueth <andreshugueth@gmail.com>
2020-09-02 14:31:18 -0500

    web flask fixing values task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/states/<state_id>', strict_slashes=False)
 def index(state_id):
     """renders an HTML with a list of states"""
-    states = storage.all("State").values()
+    states = storage.all("State")
     if state_id is not None:
         state_id = 'State.{}'.format(state_id)
     return render_template('9-states.html', states=states, state_id=state_id)
